83ee88335887997b71244db575ee57386156f09a
JustSleepTilSunSet <cloverchen.042@gmail.com>
2023-10-22 10:32:30 +0800

    Modify public path and retry the page deployment.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'testing' ? '/book-management/' : '/',
+  publicPath: process.env.NODE_ENV === 'testing' ? '/book-management/#/' : '/',
   transpileDependencies: true
 })
